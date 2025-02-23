31d0d941b0fc342e1e392d6b5d45ba908303d62f
Miguel García <migueltonio@gmail.com>
2025-02-23 03:45:12 +0100

    pend_fix_fd_in_out

--- src/pipex.c
+++ src/pipex.c
@@ -6,7 +6,7 @@
 /*   By: miggarc2 <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/06 19:02:08 by miggarc2          #+#    #+#             */
-/*   Updated: 2025/02/21 21:07:10 by miggarc2         ###   ########.fr       */
+/*   Updated: 2025/02/23 03:42:29 by miggarc2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,9 +50,12 @@ char	**ft_cmd_check(char **folders, char *av)
 	args = ft_split(av, ' ');
 	while (!cmd && *folders)
 	{
-		tmp = ft_strjoin(*folders++, "/");
-		cmd = ft_strjoin(tmp, args[0]);
-		free(tmp);
+		if (strncmp(args[0], "/", 1))
+		{
+			tmp = ft_strjoin(*folders++, "/");
+			cmd = ft_strjoin(tmp, args[0]);
+			free(tmp);
+		}
 		if (access(cmd, X_OK) < 0)
 		{
 			free(cmd);
@@ -60,11 +63,7 @@ char	**ft_cmd_check(char **folders, char *av)
 		}
 	}
 	if (cmd)
-	{
-		tmp = args[0];
-		args[0] = ft_strjoin(cmd, " <in");
-		free(tmp);
-	}
+		free(cmd);
 	else
 		perror(strerror(errno));
 	return (args);
@@ -85,7 +84,9 @@ int	main(int ac, char **av, char **env)
 		return (0);
 	var.cmds = (char ***)ft_calloc(ac - 2, sizeof(char **));
 	i = 0;
-	while (ft_strncmp(env[i], "PATH=", 5))
+	if (!env || !env[i])
+		return (0);
+	while (env[i] && ft_strncmp(env[i], "PATH=", 5))
 		i++;
 	if (!env[i])
 		return (0);
@@ -94,25 +95,34 @@ int	main(int ac, char **av, char **env)
 	while (++i < ac - 3)
 		var.cmds[i] = ft_cmd_check(var.folders, av[i + 2]);
 	i = -1;
-	while (var.cmds[++i])
+	if (var.cmds[++i])
 	{
 		if (pipe(var.pipe_in) < 0 || pipe(var.pipe_out) < 0)
 			return (0);
 		child = fork();
+		ft_printf("cmd: %s\n", var.cmds[0][0]);
 		if (child < 0)
 			return (0);
 		else if (child == 0)
 		{
 			close(var.pipe_in[0]);
 			if (dup2(var.fd_in, STDIN_FILENO) < 0 || \
-					dup2(var.fd_out, STDOUT_FILENO) < 0)
-				return (-1);
+					dup2(var.pipe_in[1], STDOUT_FILENO) < 0)
+				return (0);
+			close(var.pipe_in[1]);
 			execve(var.cmds[i][0], var.cmds[i], env);
 		}
 		else
+		{
+			waitpid(child, &status, 0);
 			close(var.pipe_in[1]);
-		wait(&status);
-		ft_printf("OK%d", status);
+//			ft_printf("status: %d", status);
+			if (dup2(var.pipe_in[0], STDIN_FILENO) < 0 || \
+					dup2(var.fd_out, STDOUT_FILENO) < 0)
+				return (0);
+			i++;
+			execve(var.cmds[i][0], var.cmds[i], env);
+		}
 	}
 	ft_clean_mem(&var);
 	close(var.fd_in);
